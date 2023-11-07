77816784b6c05eedb2a2732f6732ef4f180641fe
Thiago Sousa <thiagosousa81@gmail.com>
2023-11-07 20:28:30 -0300

    §

--- aula01/scr/example.js
+++ aula01/scr/example.js
@@ -42,5 +42,35 @@ console.log(typeof meuNumero);
 const meuNumeroConvertido = parseInt(meuNumero);
 console.log(typeof meuNumeroConvertido);
 console.log(typeof String(meuNumeroConvertido));
+
+// Condições if, else if, else
+
+const idade = 20
+
+if (idade < 13){
+	console.log("Criança");
+}
+else if (idade < 18) {
+	console.log("Adolescente");
+}
+else 
+{
+	console.log("Adulto");
+}
+
+// Programa para avaliar média de alunos
+
+const nota1 = 7.7;
+const nota2 = 8.5;
+const nota3 = 3.7;
+
+var mediaPonderada = ((nota1 * 3) + (nota2 * 3) + (nota3 * 4)) / 3 + 3 + 4;
+
+if(mediaPonderada > 6){
+	console.log("Aluno aprovado!");
+}
+else{
+	console.log("Aluno reprovado");
+}
     
 </script>
