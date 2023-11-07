5328b365728c7c83677b9224b675ae1bbc01c251
Thiago Sousa <thiagosousa81@gmail.com>
2023-11-07 19:49:04 -0300

    §

--- aula01/scr/example.js
+++ aula01/scr/example.js
@@ -1,8 +1,36 @@
-let x = 2   //int
+<script>
+let dois = 2   //int
 let nome = "Fulano";    //string
 const pi = 3.14; //constante
 var binary = true; // boolean
+let Objeto = {}; //Objeto
+let Array = []; //Array
+let nulo = null;    //Constante vazia
+let indefinido = undefined;     // Constante indefinida
+
 
 // Consoles
-console.log(x);
+console.log(dois);
 console.log(nome);
+console.log(binary)
+document.write(typeof Objeto);
+
+
+//Operadores aritméticos
+var x = 10;
+var y = 5;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
+
+// Operadores de comparação
+console.log(x == y);
+console.log(x != y);
+console.log(x > y);
+console.log(x < y);
+
+//
+    
+</script>
